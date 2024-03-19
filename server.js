af1cd928f197593f9e59d7cfbcf00cfae95b77ac
Nikhil Dasar <kakashihatakesh6@gmail.com>
2024-03-19 16:08:31 +0530

    ss

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const connectToMongo = require("./db");
-const express = require('express')
-var cors = require('cors')
+const express = require('express');
+const http = require('http');
+var cors = require('cors');
 
 connectToMongo();
 const app = express();
 const PORT = process.env.PORT || 5000 ;
 // const port = 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
 
 // Available Routes
-app.get('/', (req, res) => {
+app.use('/', (req, res) => {
     res.json({message: "Hello from server!"});
 });
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 
-app.listen(PORT, () => {
-    console.log(`Mark Attendance backend listening on port http://localhost:${PORT}`)
+server.listen(PORT, () => {
+    console.log(`Mark Attendance backend listening on port http://localhost:${PORT}`);
 });
 
 module.exports = app;
